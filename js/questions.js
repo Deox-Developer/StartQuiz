55ea1123bef434109266e37f2a37b6b562345d1f
Jonathan Zambrano <deox.desing@gmail.com>
2023-06-09 00:43:25 -0500

    fix complete

---
+++ js/questions.js
@@ -0,0 +1,97 @@
+let rutasImagenes = [
+  "imagenes/CUYABENO .jpeg",
+  "imagenes/laguna.jpeg",
+  "imagenes/limoncocha.jpeg",
+  "imagenes/Papallacta .jpeg",
+  "imagenes/Square.jpeg",
+  "imagenes/YASUNÍ.jpeg",
+];
+
+// creando una matriz y pasando el número, preguntas, opciones y respuestas
+
+let questions = [
+  {
+    numb: 1,
+    question: "What tourist place is this?",
+    imagen: [rutasImagenes[0]],
+    answer: "Cuyabeno river Lodge",
+    options: [
+      "Papallacta hot springs",
+      "Limoncocha Ecological Reserve",
+      "Cuyabeno river Lodge",
+      "National Park Yasuní",
+      "Independence Square",
+      "Blue Lagoon"    
+    ],
+  },
+  {
+    numb: 2,
+    question: "What tourist place is this?",
+    imagen: [rutasImagenes[1]],
+    answer: "Blue Lagoon",
+    options: [
+      "Papallacta hot springs",
+      "Limoncocha Ecological Reserve",
+      "Cuyabeno river Lodge",
+      "National Park Yasuní",
+      "Independence Square",
+      "Blue Lagoon"    
+    ],
+  },
+  {
+    numb: 3,
+    question: "What tourist place is this?",
+    imagen: [rutasImagenes[2]],
+    answer: "Limoncocha Ecological Reserve",
+    options: [
+      "Papallacta hot springs",
+      "Limoncocha Ecological Reserve",
+      "Cuyabeno river Lodge",
+      "National Park Yasuní",
+      "Independence Square",
+      "Blue Lagoon"    
+    ],
+  },
+  {
+    numb: 4,
+    question: "What tourist place is this?",
+    imagen: [rutasImagenes[3]],
+    answer: "Papallacta hot springs",
+    options: [
+      "Papallacta hot springs",
+      "Limoncocha Ecological Reserve",
+      "Cuyabeno river Lodge",
+      "National Park Yasuní",
+      "Independence Square",
+      "Blue Lagoon"    
+    ],
+  },
+  {
+    numb: 5,
+    question: "What tourist place is this?",
+    imagen: [rutasImagenes[4]],
+    answer: "Independence Square",
+    options: [
+      "Papallacta hot springs",
+      "Limoncocha Ecological Reserve",
+      "Cuyabeno river Lodge",
+      "National Park Yasuní",
+      "Independence Square",
+      "Blue Lagoon"    
+    ],
+  },
+  {
+    numb: 6,
+    question: "What tourist place is this?",
+    imagen: [rutasImagenes[5]],
+    answer: "National Park Yasuní",
+    options: [
+      "Papallacta hot springs",
+      "Limoncocha Ecological Reserve",
+      "Cuyabeno river Lodge",
+      "National Park Yasuní",
+      "Independence Square",
+      "Blue Lagoon"    
+    ],
+  },
+];
